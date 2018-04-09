7cee3aeb8a1c619ebf2d71d1d4f7071ca961632f
Joe Morgan <jsmapr1@gmail.com>
2018-04-09 07:40:13 -0500

    small example

--- src/components/Small/Small.js
+++ src/components/Small/Small.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import './rating.css';
+import Ansel from './ansel.jpg';
 
-export default function Small() {
+function getRating(count) {
+  return Array(count).fill('').map(item => {
+    return {
+      rating: Math.ceil(Math.random() * 4),
+      reviewer: ['Mike', 'Joe', 'Bill', 'Evan', 'Brian', 'Frank'][Math.floor(Math.random() * 6)]
+    }
+  })
+}
+
+const defaultPhoto = {
+  src: Ansel,
+  description: {
+    name: 'Tetons and Snake River',
+    photographer: 'Ansel Adams',
+    location: 'Tetons'
+  },
+  ratings: getRating(10)
+}
+
+export default function Small({ photo = defaultPhoto}) {
   return(
-    <h1>Hello</h1>
+    <div>
+      <img src={photo.src}/>
+      <div className="Description">
+        <ul>
+          {Object.keys(photo.description).map(key => {
+            return (<li key={key}>{key}: {photo.description[key]}</li>)
+          })}
+        </ul>
+      </div>
+      <div>
+        {photo.ratings.map(rating => {
+          return (
+            <div className="rating">
+              <h3>Rating</h3>
+              {Array(rating.rating).fill(<span />)}
+              <div>Reviewer: {rating.reviewer}</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
